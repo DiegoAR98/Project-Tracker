775b734cf2660671fb7faba4b9376007bafdbeb2
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2023-11-18 17:16:38 -0500

    Added better css

--- assets/script.js
+++ assets/script.js
@@ -4,46 +4,73 @@ $(document).ready(function() {
         $('#current-time').text(dayjs().format('MMM D, YYYY h:mm:ss A'));
     }, 1000);
 
+    // Function to sort projects by due date
+    function sortProjects(projects) {
+        return projects.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+    }
+
+    // Function to get the CSS class based on project type
+    function getProjectClass(type) {
+        switch (type) {
+            case 'College': return 'college';
+            case 'Internship': return 'internship';
+            case 'Extracurricular activities': return 'activities';
+            default: return '';
+        }
+    }
+
     // Function to print project data to the page
     function printProjects() {
-        var projects = JSON.parse(localStorage.getItem('projects')) || [];
+        var projects = sortProjects(JSON.parse(localStorage.getItem('projects')) || []);
         var tbody = $('#project-table-body');
-        tbody.empty(); // Clear the table body
+        tbody.empty();
 
         projects.forEach(function(project, index) {
-            var tr = $('<tr>');
+            var tr = $('<tr>').addClass(getProjectClass(project.type));
             var dueDate = dayjs(project.dueDate);
             var today = dayjs();
 
-            // Apply class based on due date status
-            if (dueDate.isBefore(today)) {
-                tr.addClass('table-danger'); // Past due date
-            } else if (dueDate.isSame(today, 'day')) {
-                tr.addClass('table-warning'); // Due today
+            if (project.status === 'done') {
+                tr.css({
+                    'background': 'linear-gradient(to left, #28a745, #6fda92)', // Apply green gradient for done projects
+                    'color': '#000000'
+                });
+            } else {
+                if (dueDate.isBefore(today)) {
+                    tr.addClass('table-danger');
+                } else if (dueDate.isSame(today, 'day')) {
+                    tr.addClass('table-warning');
+                }
             }
 
-            // Append project details to the row
             tr.append('<td>' + project.name + '</td>');
             tr.append('<td>' + project.type + '</td>');
             tr.append('<td>' + dueDate.format('MMM D, YYYY') + '</td>');
 
-            // Create a delete button
             var deleteButton = $('<button>')
                 .addClass('btn btn-danger btn-sm')
                 .text('Delete')
-                .data('index', index) // Store the index of the project
+                .data('index', index)
                 .on('click', function() {
                     var projectIndex = $(this).data('index');
-                    projects.splice(projectIndex, 1); // Remove the project
-                    localStorage.setItem('projects', JSON.stringify(projects)); // Update localStorage
+                    projects.splice(projectIndex, 1);
+                    localStorage.setItem('projects', JSON.stringify(projects));
+                    printProjects();
+                });
+
+            var doneButton = $('<button>')
+                .addClass('btn btn-success btn-sm')
+                .text('Done')
+                .data('index', index)
+                .on('click', function() {
+                    projects[index].status = 'done'; // Update status to 'done'
+                    localStorage.setItem('projects', JSON.stringify(projects)); // Save updated projects to local storage
                     printProjects(); // Reprint the projects
                 });
 
-            // Append the delete button to the row
-            var td = $('<td>').append(deleteButton);
+            var td = $('<td>').append(deleteButton, ' ', doneButton);
             tr.append(td);
 
-            // Append the row to the table body
             tbody.append(tr);
         });
     }
@@ -52,29 +79,25 @@ $(document).ready(function() {
     $('#project-form').on('submit', function(e) {
         e.preventDefault();
 
-        // Capture form data
         var projectName = $('#project-name').val();
         var projectType = $('#project-type').val();
         var projectDueDate = $('#project-due-date').val();
 
-        // Store the project data
         var projectData = {
             name: projectName,
             type: projectType,
-            dueDate: projectDueDate
+            dueDate: projectDueDate,
+            status: 'pending' // Default status as 'pending'
         };
 
-        // Retrieve the current list of projects and add the new project
         var projects = JSON.parse(localStorage.getItem('projects')) || [];
         projects.push(projectData);
         localStorage.setItem('projects', JSON.stringify(projects));
 
-        // Print the projects and reset the form
         printProjects();
         $('#projectModal').modal('hide');
         $(this).trigger('reset');
     });
 
-    // Print projects on page load
     printProjects();
 });
